d8c50870c29b82baf75b32122942baccf6f580bf
ngovanthinh13 <65067636+ngovanthinh13@users.noreply.github.com>
2021-05-05 09:11:18 +0700

    Create check_classname_in_element.js
    check classname trong element

---
+++ check_classname_in_element.js
@@ -0,0 +1,8 @@
+<div class="secondary info">Item</div>
+
+
+const div = document.querySelector('div');
+div.classList.contains('secondary'); // true
+
+const div = document.querySelector('div');
+div.classList.contains('error'); // false
