74755a459040a6663542885c490449fc53f4cfbc
ngovanthinh13 <65067636+ngovanthinh13@users.noreply.github.com>
2021-05-12 06:39:46 +0700

    Create js_advantage.js
    shadow và mảng

---
+++ js_advantage.js
@@ -0,0 +1,22 @@
+// cach mo shadow cho root - nó là cái bóng của DOM dùng xây dựng các app dựa trên các component
+constructor(){
+  supper()
+  this.attactShadow({mode: 'open'}) //đây là cách mở khai báo shadow
+}
+
+this.shadowRoot.innerHTML= ` `; //đây là cách dùng
+
+//-------------------------------------------------------------------------
+array.prototype.includes(index) //trả về giá trị true - false
+
+//-------------------------------------------------------------------------
+array.slice(index) //trích xuất phần tử trong mảng
+
+//-------------------------------------------------------------------------
+array.indexOf(item, start)
+
+// Trong đó:
+
+// item là giá trị của phần tử cần tìm.
+// start là vị trí bắt đầu tìm kiếm.
+//khhi không tìm thấy trả về giá trị -1
