b553f01b7af7c6ae86bd85eb438b9bc9cd09c0a0
ngovanthinh13 <65067636+ngovanthinh13@users.noreply.github.com>
2021-05-16 12:23:27 +0700

    Create oposition_ele.js
    tim vi tri cua doi tuong

---
+++ oposition_ele.js
@@ -0,0 +1,15 @@
+var a = document.querySelector('input')
+//undefined
+var l = document.getElementById('last-name')
+//undefined
+l.getBoundingClientRect()
+// DOMRect {x: -20, y: 272.8000183105469, width: 440.4250183105469, height: 32, top: 272.8000183105469, …}
+//bottom: 304.8000183105469
+//height: 32
+//left: -20
+//right: 420.4250183105469
+//top: 272.8000183105469
+//width: 440.4250183105469
+//x: -20
+//y: 272.8000183105469
+//__proto__: DOMRect
